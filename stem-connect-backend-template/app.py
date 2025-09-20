1afc2e588d2dfd6d53780118afec0d508cec4ff7
Andrew Ye <andrewye2005@gmail.com>
2025-09-20 16:52:32 -0400

    fist commit

--- stem-connect-backend-template/app.py
+++ stem-connect-backend-template/app.py
@@ -12,6 +12,3 @@ def home():
 def get_message():
     message = {"message": "This is a message from the backend."}
     return jsonify(message)
-
-if __name__ == '__main__':
-    app.run(debug=True)
